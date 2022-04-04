5855fde5c58fa19bfb5bd3c94493011cdd9d273b
lewiis252 <krystianoleniacz@gmail.com>
2022-04-04 15:08:00 +0200

    improvements

--- metoda_stycznych.py
+++ metoda_stycznych.py
@@ -11,11 +11,13 @@ def metoda_stycznych(f = lambda x: x**4-2*x-4, f_prim = lambda x: 4*x**3-2, dokl
         index = 1
         while abs(round(f(x[index-1]), miejsc_po_przecinku)) > dokladnosc:
             print(f'x_{index-1} = {x[index-1]}')
+            print(f'f(x_{index-1}) = {round(f(x[index-1]), miejsc_po_przecinku)}')
             x_new = round(x[index-1] - f(x[index-1])/f_prim(x[index-1]),miejsc_po_przecinku)
             x.append(x_new)
             index = index + 1
 
         print(f'x_{index - 1} = {x[index - 1]}')
+        print(f'f(x_{index - 1}) = {round(f(x[index-1]), miejsc_po_przecinku)}')
         print(f'Lista przybliżeń: {x}')
 
 metoda_stycznych()
