a1b0f12f149647cd738342bc5aa5c8500808500d
lewiis252 <krystianoleniacz@gmail.com>
2022-04-04 14:46:52 +0200

    works great

---
+++ metoda_stycznych.py
@@ -0,0 +1,22 @@
+def metoda_stycznych(f = lambda x: x**4-2*x-4, f_prim = lambda x: 4*x**3-2, dokladnosc=0.01, lewy_koniec=0, prawy_koniec=1.7, miejsc_po_przecinku = 4):
+    '''Oblicza przybliżone miejsce zerowe funkcji na zadanym przedziale'''
+
+    if f(lewy_koniec) * f(prawy_koniec) > 0:
+        print('Brak miejsc zerowych na przedziale.')
+        return False
+    else:
+        x_0 = prawy_koniec
+
+        x_1 = round(x_0 - f(x_0)/f_prim(x_0), miejsc_po_przecinku)
+        x = [x_0]
+        index = 1
+        while abs(round(f(x[index-1]), miejsc_po_przecinku)) > dokladnosc:
+            print(f'x_{index-1} = {x[index-1]}')
+            x_new = round(x[index-1] - f(x[index-1])/f_prim(x[index-1]),miejsc_po_przecinku)
+            x.append(x_new)
+            index = index + 1
+
+        print(f'x_{index - 1} = {x[index - 1]}')
+        print(f'Lista przybliżeń: {x}')
+
+metoda_stycznych()
